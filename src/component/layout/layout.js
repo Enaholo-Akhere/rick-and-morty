6bab1e94a8050aa776aadb3e0d1e8c4f00af356e
Enaholo Akhere <enaholoa@gmail.com>
2022-10-08 21:33:49 +0100

    removed unused imports

--- src/component/layout/layout.js
+++ src/component/layout/layout.js
@@ -3,7 +3,6 @@ import NavBar from '../navbar.js/navbar';
 import Sidebar from '../sidebar/sidebar';
 import { useState } from 'react';
 import ScrollTo from '../../utilities/scroll-to';
-import { useEffect } from 'react';
 const Layout = (props) => {
   const [open, onClose] = useState(false);
 
